3a0967b0b1de6e15504a58b0099da7d37f8d1cf2
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-09-09 23:51:36 +0800

    fpisolve: do a different one

--- 01_fpisolve/main.py
+++ 01_fpisolve/main.py
@@ -1,30 +1,36 @@
 import numpy as np
 
-#
-# Start small
-# A function: f(a, b) = 2a + 9b
-#
-
 # Values at the diagonal must be large
 # Does not converge otherwise
 A = np.array([
-	[0.91, 1.04, 0.19],
-	[0.04, -.99, -.85],
-	[0.10, -.07, -.96]
+	[0.60, 0.02, 0.99],
+	[0.99, 0.01, 0.07],
+	[0.06, 0.92, 0.03]
 ])
 
-B = np.array([-1.67, -3.73, -2.04])
+B = np.array([-0.98, 0.66, -.82])
 
 ans = np.linalg.solve(A, B)
 print("Reference answer", ans)
 
+def rearrange(A):
+	for i in range(len(A)):
+		idx = np.argmax(A[i:, i])
+		A[i], A[i+idx] = A[i+idx].copy(), A[i].copy()
+		B[i], B[i+idx] = B[i+idx].copy(), B[i].copy()
+
+rearrange(A)
+
+#
+# Solving for unknowns
 #
 # 2u + 1v = 20
 # 3u + 5v = 51
 #
-
 # u = (20 - 1v) / 2
 # v = (51 - 5v) / 3
+#
+
 n = len(A)
 diag = np.diag(A)
 
